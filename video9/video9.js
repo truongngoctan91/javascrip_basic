3e7d89666092e71bfeb10c535b05f5eb6ef0cdd7
truongngoctan <truongngoctanbn@gmail.com>
2022-10-07 16:12:50 +0200

    video9

---
+++ video9/video9.js
@@ -0,0 +1,16 @@
+console.log("Hello world from HTML")
+let a = {
+    name: "Tan",
+    age : "30"
+}; // key :value
+console.log("I'm ", a.age)
+
+let b = ["Mu", "Chelsea", "Liver"];
+console.log("Typeofb", typeof b, b)
+console.log('first item:', b[0], 'second item', b[1])
+let family= [
+    { name: "Tan", age : 25},
+    { name : "Hoai", age: 26}
+];
+
+console.log("My family:", family)
