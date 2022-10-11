623d40441d1e09ff395e0e548c0a6bdda5558f45
truongngoctan <truongngoctanbn@gmail.com>
2022-10-11 11:09:05 +0200

    video12

---
+++ video12/video12.js
@@ -0,0 +1,15 @@
+console.log("Hello world from HTML111")
+let arrtop4 = ["Man city", "Livepool", 'Mu', 'Chelsea'];
+let a = "Livepool";
+let b = 5;
+let i = 1;
+function get_top(acb) {
+
+    if (acb[i] === "Livepool") {
+        b+=10;
+        return;
+    }
+    return;
+}
+get_top(arrtop4);
+console.log("return of get_top:", b)
